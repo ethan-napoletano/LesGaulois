7110d2f5fb634d0aaefa27340cfc6dc36230e203
Ethan Napoletano <npt4866a@ensfsi.onmicrosoft.com>
2024-02-15 11:35:13 +0100

    classe gaulois

---
+++ src/personnages/Chef.java
@@ -0,0 +1,16 @@
+package personnages;
+
+public class Chef {
+	private String nom;
+	private int force;
+	private Village village;
+	public Chef(String nom, int force, Village village) {
+		this.nom = nom;
+		this.force = force;
+		this.village = village;
+	}
+	
+	
+
+
+}
