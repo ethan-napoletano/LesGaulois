1b8f135ef31e40c34f896d4f8517bffffcd814e6
Ethan Napoletano <npt4866a@ensfsi.onmicrosoft.com>
2024-02-15 11:55:45 +0100

    partie 2

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -23,8 +23,15 @@ public class Gaulois {
 	}
 
 	public void frapper(Romain romain) {
-		System.out.println(nom + "envoie un grand coup dans la machoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+				+ romain.getNom());
+		romain.recevoirCoup(force/3);
+	}
+	
+	@Override
+	public String toString() {
+	return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion="
+	+ effetPotion + "]";
 	}
 
 	public static void main(String[] args) {
@@ -32,7 +39,12 @@ public class Gaulois {
 //TODO créer un main permettant de tester la classe Gaulois
 		
 		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
 		System.out.println(asterix);
+		System.out.println(asterix.prendreParole());
+		asterix.parler("bonjour");
+		
+		
 		
 	}
 
